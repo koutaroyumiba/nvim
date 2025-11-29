b0726d47a0c0eed06eb2a13af6237e7fdbbc9f39
Kot <89110272+Kot6603@users.noreply.github.com>
2025-11-30 01:10:56 +1300

    feat: lualine moved to new structure

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+					component_separators = "",
+					section_separators = "",
+				},
+				sections = {
+					lualine_b = { "branch" },
+					lualine_c = { { "filename", path = 3 }, "diagnostics" },
+					lualine_x = { "diff", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = { "filename" },
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = { "location" },
+				},
+				tabline = {},
+				extensions = {},
+			})
+		end,
+	},
+}
