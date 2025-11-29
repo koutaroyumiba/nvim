900d70d4a2c6371d8e20bc388beb2ef60f511771
Kot <89110272+Kot6603@users.noreply.github.com>
2025-11-30 01:11:59 +1300

    feat: mini nvim (quality of life) plugins

---
+++ lua/plugins/mini-nvim.lua
@@ -0,0 +1,8 @@
+-- cool small plugins
+return {
+    { "nvim-mini/mini.trailspace", version = "*", opts = {} },
+    { "nvim-mini/mini.comment", version = "*", opts = {} },
+    { "nvim-mini/mini.surround", version = "*", opts = {} },
+    { "nvim-mini/mini.notify", version = "*", opts = {} },
+    { "nvim-mini/mini.indentscope", version = "*", opts = {} },
+}
