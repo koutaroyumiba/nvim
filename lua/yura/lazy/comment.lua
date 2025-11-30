b16a0df426503692dad1f8040b70b0ae7322ef72
Kot <89110272+Kot6603@users.noreply.github.com>
2025-11-30 20:13:24 +1300

    chore: deleted unnecessary files

--- lua/yura/lazy/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-    "numToStr/Comment.nvim",
-    config = function()
-      local comment = require("Comment")
-      comment.setup()
-    end
-  },
-}
--- lua/yura/lazy/init.lua
+++ lua/yura/lazy/init.lua
@@ -1,20 +1,10 @@
 return {
-	{ "nvim-lua/plenary.nvim" },
-	{ "christoomey/vim-tmux-navigator" },
-	{ "szw/vim-maximizer" },
-	{ "tpope/vim-surround" },
-	{ "mbbill/undotree" }, -- undo tree
-	{ "simrat39/symbols-outline.nvim" },
+	{ "nvim-lua/plenary.nvim" }, -- not needed i think
+	{ "christoomey/vim-tmux-navigator" }, -- done
+	{ "szw/vim-maximizer" }, -- dont really need...
+	{ "tpope/vim-surround" }, -- done
+	{ "mbbill/undotree" },
 	{ "windwp/nvim-ts-autotag" },
 	{ "theprimeagen/vim-be-good" },
 	{ "github/copilot.vim" },
 }
-
--- LSP support
--- 			{ "jay-babu/mason-nvim-dap.nvim" },
--- 			{ "honza/vim-snippets" },
--- 			{ "rafamadriz/friendly-snippets" },
--- 	{ "kdheepak/lazygit.nvim" },
--- 	-- { "vim-scripts/ReplaceWithRegister" },
---
--- -- use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
