c45ad2a2fda38c8fd853e4684b973ebc7b264879
Kot <89110272+Kot6603@users.noreply.github.com>
2026-01-20 23:21:03 +1100

    feat: indenting?

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,64 +1,71 @@
 return {
-    "ibhagwan/fzf-lua",
-    lazy = false,
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-        {
-            "<leader>ff",
-            function()
-                require("fzf-lua").files()
-            end,
-            desc = "fzf files in cwd",
-        },
-        {
-            "<leader>fs",
-            function()
-                require("fzf-lua").live_grep()
-            end,
-            desc = "fzf live grep in cwd",
-        },
-        {
-            "<leader>fb",
-            function()
-                require("fzf-lua").buffers()
-            end,
-            desc = "fzf buffers",
-        },
-        {
-            "<leader>fh",
-            function()
-                require("fzf-lua").help_tags()
-            end,
-            desc = "fzf help tags",
-        },
-        {
-            "<leader>fx",
-            function()
-                require("fzf-lua").diagnostics_document()
-            end,
-            desc = "fzf diagnostics document",
-        },
-        {
-            "<leader>fX",
-            function()
-                require("fzf-lua").diagnostics_workspace()
-            end,
-            desc = "fzf diagnostics workspace",
-        },
-        {
-            "<leader>gc",
-            function()
-                require("fzf-lua").git_commits()
-            end,
-            desc = "fzf git commits",
-        },
-        {
-            "<leader>gb",
-            function()
-                require("fzf-lua").git_branches()
-            end,
-            desc = "fzf git branches",
-        },
-    },
-    opts = {},
+	"ibhagwan/fzf-lua",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "fzf files in cwd",
+		},
+		{
+			"<leader>fs",
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "fzf live grep in cwd",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "fzf buffers",
+		},
+		{
+			"<leader>fh",
+			function()
+				require("fzf-lua").help_tags()
+			end,
+			desc = "fzf help tags",
+		},
+		{
+			"<leader>fm",
+			function()
+				require("fzf-lua").manpages()
+			end,
+			desc = "fzf manpages",
+		},
+		{
+			"<leader>fx",
+			function()
+				require("fzf-lua").diagnostics_document()
+			end,
+			desc = "fzf diagnostics document",
+		},
+		{
+			"<leader>fX",
+			function()
+				require("fzf-lua").diagnostics_workspace()
+			end,
+			desc = "fzf diagnostics workspace",
+		},
+		{
+			"<leader>gc",
+			function()
+				require("fzf-lua").git_commits()
+			end,
+			desc = "fzf git commits",
+		},
+		{
+			"<leader>gb",
+			function()
+				require("fzf-lua").git_branches()
+			end,
+			desc = "fzf git branches",
+		},
+	},
+	opts = {},
 }
