00a8f6f3524bbc349c0f22d37fc7202d81ca9265
Kot <89110272+Kot6603@users.noreply.github.com>
2026-01-20 23:50:52 +1100

    feat: treesitter update!!

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,40 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    event = {"BufReadPost", "BufNewFile"},
-    branch = "master",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "json",
-                "javascript",
-                "typescript",
-                "tsx",
-                "yaml",
-                "html",
-                "css",
-                "markdown",
-                "markdown_inline",
-                "svelte",
-                "graphql",
-                "go",
-                "bash",
-                "lua",
-                "vim",
-                "dockerfile",
-                "gitignore",
-                "cpp",
-                "c",
-            },
-            auto_install = true,
-            sync_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"go",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"cpp",
+				"c",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "<filetype>" },
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
