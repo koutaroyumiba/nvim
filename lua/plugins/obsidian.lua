2b9fe2b0393fd52720a505faefe95d0605fe91dc
Kot <89110272+Kot6603@users.noreply.github.com>
2026-01-20 23:53:03 +1100

    feat: obsidian daily notes

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -13,6 +13,19 @@ return {
 				path = "~/my_obsidian/kot-pkm",
 			},
 		},
+		daily_notes = {
+			folder = "Daily Notes",
+			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+			template = "Daily Note Template.md",
+		},
+		-- Optional, for templates (see below).
+		templates = {
+			folder = "Templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M",
+			-- A map for custom variables, the key should be the variable and the value a function
+			substitutions = {},
+		},
 		ui = { enable = false },
 		disable_frontmatter = true,
 		open_app_foreground = true,
