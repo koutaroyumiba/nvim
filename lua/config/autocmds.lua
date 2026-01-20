4f00f504c69f96a5a42026075db0034041545944
Kot <89110272+Kot6603@users.noreply.github.com>
2026-01-21 00:04:44 +1100

    feat: no more efm lsp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,51 +1,38 @@
 -- restore last cursor position when reopening a file
 local last_cursor_group = vim.api.nvim_create_augroup("LastCursorGroup", {})
 vim.api.nvim_create_autocmd("BufReadPost", {
-    group = last_cursor_group,
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
-    end,
+	group = last_cursor_group,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
 
 -- highlight the yanked text for 100ms
 local highlight_yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = highlight_yank_group,
-    pattern = "*",
-    callback = function()
-        vim.hl.on_yank({ higroup = "IncSearch", timeout = 100 })
-    end,
-})
-
--- format on save using efm langserver and configured formatters
-local lsp_fmt_group = vim.api.nvim_create_augroup("FormatOnSaveGroup", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-    group = lsp_fmt_group,
-    callback = function()
-        local efm = vim.lsp.get_clients({ name = "efm" })
-        if vim.tbl_isempty(efm) then
-            return
-        end
-        vim.lsp.buf.format({ name = "efm", async = true })
-    end,
+	group = highlight_yank_group,
+	pattern = "*",
+	callback = function()
+		vim.hl.on_yank({ higroup = "IncSearch", timeout = 100 })
+	end,
 })
 
 -- on attach function shortcuts
 local on_attach = require("utils.lsp").on_attach
 local lsp_on_attach_group = vim.api.nvim_create_augroup("LspMappings", {})
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = lsp_on_attach_group,
-    callback = on_attach,
+	group = lsp_on_attach_group,
+	callback = on_attach,
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-    group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-    callback = function()
-        vim.opt.number = false
-        vim.opt.relativenumber = false
-    end,
+	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end,
 })
