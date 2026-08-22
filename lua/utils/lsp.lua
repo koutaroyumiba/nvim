c32c8cdf1782077b290b7ae479c7c595ee8dd0dc
Kot <89110272+koutaroyumiba@users.noreply.github.com>
2026-08-23 01:31:13 +1200

    chore: removed deprecated functions

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -14,20 +14,20 @@ M.on_attach = function(event)
 	}
 
 	-- native neovim keymaps
-	keymap("n", "gd", vim.lsp.buf.definition, opts) -- go to definition
-	keymap("n", "gD", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts) -- go to definition in split
-	keymap("n", "<leader>vca", vim.lsp.buf.code_action, opts) -- selects an available code action
-	keymap("n", "<leader>vrn", vim.lsp.buf.rename, opts) -- rename symbol
-	keymap("n", "<leader>vd", vim.diagnostic.open_float, opts) -- cursor diagnostics
+	keymap("n", "gd", vim.lsp.buf.definition, opts)                                            -- go to definition
+	keymap("n", "gD", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)                  -- go to definition in split
+	keymap("n", "<leader>vca", vim.lsp.buf.code_action, opts)                                  -- selects an available code action
+	keymap("n", "<leader>vrn", vim.lsp.buf.rename, opts)                                       -- rename symbol
+	keymap("n", "<leader>vd", vim.diagnostic.open_float, opts)                                 -- cursor diagnostics
 	keymap("n", "<leader>vD", "<cmd>lua vim.diagnostic.open_float({ scope = 'line' })<CR>", opts) -- line diagnostics
-	keymap("n", "<leader>pd", vim.diagnostic.goto_prev, opts) -- previous diagnostic
-	keymap("n", "<leader>nd", vim.diagnostic.goto_next, opts) -- next diagnostic
-	keymap("n", "K", vim.lsp.buf.hover, opts) -- displays hover information about symbol under cursor
+	keymap("n", "<leader>pd", function() vim.diagnostic.jump({ count = -1 }) end, opts)        -- previous diagnostic
+	keymap("n", "<leader>nd", function() vim.diagnostic.jump({ count = 1 }) end, opts)         -- next diagnostic
+	keymap("n", "K", vim.lsp.buf.hover, opts)                                                  -- displays hover information about symbol under cursor
 
 	-- fzf-lua keymaps
-	keymap("n", "<leader>fd", "<cmd>FzfLua lsp_finder<CR>", opts) -- lsp finder (definitions + references)
-	keymap("n", "<leader>fr", "<cmd>FzfLua lsp_references<CR>", opts) -- show all references to symbol under cursor
-	keymap("n", "<leader>ft", "<cmd>FzfLua lsp_typedefs<CR>", opts) -- jump to the typedefs of symbol under cursor
+	keymap("n", "<leader>fd", "<cmd>FzfLua lsp_finder<CR>", opts)         -- lsp finder (definitions + references)
+	keymap("n", "<leader>fr", "<cmd>FzfLua lsp_references<CR>", opts)     -- show all references to symbol under cursor
+	keymap("n", "<leader>ft", "<cmd>FzfLua lsp_typedefs<CR>", opts)       -- jump to the typedefs of symbol under cursor
 	keymap("n", "<leader>ds", "<cmd>FzfLua lsp_document_symbols<CR>", opts) -- list all symbols in file
 	keymap("n", "<leader>ws", "<cmd>FzfLua lsp_workspace_symbols<CR>", opts) -- search for symbol across entire project
 	keymap("n", "<leader>fi", "<cmd>FzfLua lsp_implementations<CR>", opts) -- go to implementation
