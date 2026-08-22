6c5b14403461fdc8780bdd5a13d5aa9443715355
Kot <89110272+koutaroyumiba@users.noreply.github.com>
2026-08-23 01:19:31 +1200

    feat: new markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,7 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && yarn install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
+	"selimacerbas/markdown-preview.nvim",
+	dependencies = { "selimacerbas/live-server.nvim" },
+	config = function()
+		require("markdown_preview").setup({})
+	end
 }
